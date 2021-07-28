b16ecfd095bfae8a1d4d18696f65b6d82815b89a
windwolf <zhoujian.ww@gmail.com>
2021-07-28 18:42:40 +0800

    调整 单元测试方法

--- test/minunit.h
+++ test/minunit.h
@@ -9,36 +9,47 @@ extern "C"
 #include "stdio.h"
 
 /* file: minunit.h */
-#define MU_ASSERT(message, test)    \
-    do                              \
-    {                               \
-        if (!(test))                \
-            printf(message "\r\n"); \
+#define MU_ASSERT(test)               \
+    do                                \
+    {                                 \
+        if (!(test))                  \
+            printf("TEST: "           \
+                   "(" __FILE__ ":%d" \
+                   ":" #test ")\r\n", \
+                   __LINE__);         \
     } while (0)
 
-#define MU_RUN(test)                \
-    do                              \
-    {                               \
-        char *message = test();     \
-        if (message)                \
-            printf(message "\r\n"); \
+#define MU_RUN(test)                  \
+    do                                \
+    {                                 \
+        if (!test())                  \
+            printf("TEST: "           \
+                   "(" __FILE__ ":%d" \
+                   ":" #test ")\r\n", \
+                   __LINE__);         \
     } while (0)
 
-#define MU_ASSERT_EQUALS(message, a, b) \
-    do                                  \
-    {                                   \
-        if (a != b)                     \
-            printf(message "\r\n");     \
+#define MU_ASSERT_EQUALS(a, b)             \
+    do                                     \
+    {                                      \
+        if (a != b)                        \
+            printf("TEST: "                \
+                   "(" __FILE__ ":%d"      \
+                   ":" #a "==" #b ")\r\n", \
+                   __LINE__);              \
     } while (0)
 
-#define MU_ASSERT_VEC_EQUALS(message, a, b, size) \
-    do                                            \
-    {                                             \
-        for (uint32_t i = 0; i < size; i++)       \
-        {                                         \
-            if (a[i] != b[i])                     \
-                printf(message "\r\n");           \
-        }                                         \
+#define MU_ASSERT_VEC_EQUALS(a, b, size)               \
+    do                                                 \
+    {                                                  \
+        for (uint32_t i = 0; i < size; i++)            \
+        {                                              \
+            if (a[i] != b[i])                          \
+                printf("TEST: "                        \
+                       "(" __FILE__ ":%d"              \
+                       ":" #a "[%d]==" #b "[%d])\r\n", \
+                       __LINE__, i, i);                \
+        }                                              \
     } while (0)
 
 #define MU_VEC_CLEAR(vec, size)             \

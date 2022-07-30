12aad25a08186979ca31ad30a74a81f00f77a586
zhoujian <zhoujian.ww@gmail.com>
2022-07-31 01:40:29 +0800

    led   修正LL驱动调用问题

--- device/led.c
+++ device/led.c
@@ -1,5 +1,42 @@
 #include "led.h"
 
+static void led_enableChannel(Led *device, uint8_t chn)
+{
+    switch (chn)
+    {
+    case 1U:
+        LL_TIM_CC_EnableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH1);
+        break;
+    case 2U:
+        LL_TIM_CC_EnableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH2);
+        break;
+    case 3U:
+        LL_TIM_CC_EnableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH3);
+        break;
+    case 4U:
+        LL_TIM_CC_EnableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH4);
+        break;
+    }
+}
+static void led_disableChannel(Led *device, uint8_t chn)
+{
+    switch (chn)
+    {
+    case 1U:
+        LL_TIM_CC_DisableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH1);
+        break;
+    case 2U:
+        LL_TIM_CC_DisableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH2);
+        break;
+    case 3U:
+        LL_TIM_CC_DisableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH3);
+        break;
+    case 4U:
+        LL_TIM_CC_DisableChannel(device->tim->Instance, LL_TIM_CHANNEL_CH4);
+        break;
+    }
+}
+
 void led_create(Led *device,
                 TIM_HandleTypeDef *tim,
                 TIMER_CHANNEL redChannel,
@@ -12,14 +49,20 @@ void led_create(Led *device,
     device->bChannel = blueChannel;
 };
 
-void led_start(Led *ledDevice)
+void led_start(Led *device)
 {
-    LL_TIM_EnableCounter(ledDevice->tim->Instance);
+    // LL_TIM_EnableCounter(ledDevice->tim->Instance);
+    led_enableChannel(device, device->rChannel);
+    led_enableChannel(device, device->gChannel);
+    led_enableChannel(device, device->bChannel);
 }
 
-void led_stop(Led *ledDevice)
+void led_stop(Led *device)
 {
-    LL_TIM_DisableCounter(ledDevice->tim->Instance);
+    // LL_TIM_DisableCounter(ledDevice->tim->Instance);
+    led_disableChannel(device, device->rChannel);
+    led_disableChannel(device, device->gChannel);
+    led_disableChannel(device, device->bChannel);
 }
 
 void led_color_set(Led *device, Color color)

994e9360ec6b187d657208f86da72a43105f62d4
zhoujian <zhoujian.ww@gmail.com>
2022-12-05 09:56:56 +0800

    修复address没shift的错误

--- device/AS5600I2C.cpp
+++ device/AS5600I2C.cpp
@@ -26,7 +26,7 @@ void wibot::device::AS5600I2C::zero_set()
 Result wibot::device::AS5600I2C::_init()
 {
 	auto& cfg = _i2c.config_get();
-	cfg.slaveAddress = AS5600_I2C_ADDRESS;
+	cfg.slaveAddress = AS5600_I2C_ADDRESS << 1;
 	cfg.dataWidth = DataWidth::Bit8;
 	cfg.useRxDma = false;
 	cfg.useTxDma = false;
@@ -45,6 +45,7 @@ void wibot::device::AS5600I2C::angle_get(uint32_t& angle)
 void wibot::device::AS5600I2C::config_apply(wibot::device::AS5600I2CConfig& config)
 {
 	Configurable::config_apply(config);
+	_i2c.config_get().slaveAddress = AS5600_I2C_ADDRESS << 1;
 }
 void wibot::device::AS5600I2C::_deinit()
 {

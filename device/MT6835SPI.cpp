58dedd6e8fa5b4e9fba4cc00ded8aeff508d54a6
zhoujian <zhoujian.ww@gmail.com>
2023-02-13 09:47:59 +0800

    device   增加mt6835spi

---
+++ device/MT6835SPI.cpp
@@ -0,0 +1,10 @@
+//
+// Created by zhouj on 2023/2/12.
+//
+
+#include "MT6835SPI.hpp"
+
+namespace wibot {
+namespace device {
+} // wibot
+} // device
---
+++ device/MT6835SPI.hpp
@@ -0,0 +1,18 @@
+//
+// Created by zhouj on 2023/2/12.
+//
+
+#ifndef WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_
+#define WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_
+
+namespace wibot {
+namespace device {
+
+class MT6835SPI {
+
+};
+
+} // wibot
+} // device
+
+#endif //WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_

5cd4daadb1c2f424747dc8eece81c2f403330a93
zhoujian <zhoujian.ww@gmail.com>
2023-02-13 09:48:25 +0800

    device   + mt6835

--- device/MT6835SPI.cpp
+++ device/MT6835SPI.cpp
@@ -4,7 +4,34 @@
 
 #include "MT6835SPI.hpp"
 
-namespace wibot {
-namespace device {
-} // wibot
+#define MT6835SPI_READ_CMD 0xA0
+#define MT6835SPI_ANGLE_REG 0x03
+
+namespace wibot
+{
+    namespace device
+    {
+        Result MT6835SPI::_init()
+        {
+            INIT_BEGIN()
+            MEMBER_INIT_ERROR_CHECK(spi_);
+            INIT_END()
+        }
+        void MT6835SPI::_deinit()
+        {
+        }
+        uint32_t MT6835SPI::get_angle()
+        {
+            uint8_t data[6];
+            cmd_[0] = MT6835SPI_READ_CMD;
+            cmd_[1] = MT6835SPI_ANGLE_REG;
+            spi_.write_read(cmd_, data, 6, wh_);
+            wh_.wait(TIMEOUT_FOREVER);
+            return (data[2] << 13) | (data[3] << 5) | (data[4] >> 3);
+        }
+        uint32_t MT6835SPI::get_data()
+        {
+            return get_angle();
+        }
+    } // wibot
 } // device
--- device/MT6835SPI.hpp
+++ device/MT6835SPI.hpp
@@ -5,14 +5,46 @@
 #ifndef WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_
 #define WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_
 
-namespace wibot {
-namespace device {
+#include "base.hpp"
+#include "spi.hpp"
+#include "DataSource.hpp"
 
-class MT6835SPI {
+namespace wibot
+{
+    namespace device
+    {
+        using namespace wibot;
+        using namespace wibot::peripheral;
 
-};
+        struct MT6835SpiConfig
+        {
 
-} // wibot
+        };
+
+        class MT6835SPI : public Initializable,
+                          public Configurable<MT6835SpiConfig>,
+                          public DataSource
+        {
+         public:
+            MT6835SPI(Spi& spi, EventGroup& eg)
+                : spi_(spi), wh_(eg)
+            {
+            };
+
+            uint32_t get_angle();
+
+            uint32_t get_data() override;
+
+         protected:
+            Result _init() override;
+            void _deinit() override;
+
+            Spi& spi_;
+            uint8_t cmd_[6];
+            wibot::WaitHandler wh_;
+        };
+
+    } // wibot
 } // device
 
 #endif //WWMOTOR_LIBS_WWDEVICE_DEVICE_MT6835SPI_HPP_
